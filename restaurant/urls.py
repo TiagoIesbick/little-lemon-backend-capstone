5083b138dcec81266b72b18d9bec962b3f7af8c4
TiagoIesbick <tiagoiesbick@gmail.com>
2023-04-25 16:20:25 -0300

    setting up views and routes

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from .views import *
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
+    path('menu', MenuItemsView.as_view(), name='menu'),
+    path('menu/<int:pk>', SingleMenuItemView.as_view(), name='menu_item'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
+from .models import *
+from .serializers import *
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
